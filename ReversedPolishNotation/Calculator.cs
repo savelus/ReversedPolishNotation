57d89910a7d975c4d059a3424f8eb5e712c0af9d
Савелий <savelussava@mail.ru>
2021-04-22 10:53:18 +0500

    Add reading data for argument

---
+++ ReversedPolishNotation/Calculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReversedPolishNotation
+{
+    class Calculator
+    {
+        public static void ParseArgumentData(string[] numbers, ref double start, ref double step, ref double end)
+        {
+
+            if (!(double.TryParse(numbers[0], out start) && double.TryParse(numbers[0], out step) && double.TryParse(numbers[0], out end)))
+                throw new Exception("Данные для аргумента введены не правильно");
+
+        }
+    }
+}
